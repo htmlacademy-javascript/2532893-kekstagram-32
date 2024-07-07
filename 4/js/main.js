6e0a2f35f919065a34366196abc7bae353c69fe3
Keks <keks@htmlacademy.ru>
2024-07-07 08:56:28 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,22 +1,4 @@
+import { getArrayOfObjects } from './setup.js';
 
-import { getRandomNum } from './utils.js';
-import { getArrayOfID, getArrayOfURL, getArrayOfDescriptions, getArrayOfComments, COUNT_OF_PHOTOS} from './data.js';
-
-
-
-// Массив объектов
-const getArrayOfObjects = function(){
-  const arrayOfObjects = [];
-
-  for (let i = 0; i < COUNT_OF_PHOTOS; i++){
-    arrayOfObjects.push({
-      id: getArrayOfID()[i],
-      url: getArrayOfURL()[i],
-      description: getArrayOfDescriptions[i],
-      likes: getRandomNum(15, 200),
-      comments: getArrayOfComments()
-    });
-  }
-  return arrayOfObjects;
-};
-console.log(getArrayOfObjects());
+console.log(`Массив объектов:
+  ${getArrayOfObjects()}`);
