8da51e10b7df52c64521e83f979262b06ee3ff9c
Yuriy Andreev <andreyev.yury@gmail.com>
2024-07-25 20:06:20 +0300

    start from beginning

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import { photos } from './setup.js';
-import { generateThumbnails } from './thumbs.js';
-import { fullSizePictures } from './full-size-pictures.js';
-
-generateThumbnails(photos);
-fullSizePictures();
-
-
