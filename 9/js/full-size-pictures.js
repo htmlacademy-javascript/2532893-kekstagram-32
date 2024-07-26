340b23841efa43e15a4ae912cd04f48ad010c574
Keks <keks@htmlacademy.ru>
2024-07-26 21:07:11 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/full-size-pictures.js
+++ 9/js/full-size-pictures.js
@@ -1,85 +1,66 @@
 import { photos } from './setup.js';
-import { container } from './thumbs.js';
-import { isHidden } from './utils.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = document.querySelector('.big-picture__img').querySelector('img');
 const likesCount = document.querySelector('.likes-count');
+const socialComments = document.querySelector('.social__comments');
+const socialCommentTemplate = document.querySelector('.social__comment');
 const socialCaption = document.querySelector('.social__caption');
 const socialCommentsShownCount = document.querySelector('.social__comment-shown-count');
 const socialCommentsTotalCount = document.querySelector('.social__comment-total-count');
+const socialCommentsCount = document.querySelector('.social__comment-count');
+const socialCommentsLoader = document.querySelector('.comments-loader');
 const bigPictureCancelButton = document.querySelector('.big-picture__cancel');
-const socialComments = document.querySelector('.social__comments');
-const socialCommentTemplate = document.querySelector('.social__comment');
-const overlayPicture = document.querySelector('.overlay');
-const socialCommentCount = document.querySelector('.social__comment-count');
-const commentsLoader = document.querySelector('.comments-loader');
+const quantityOfShownComments = countShowComments();
 
-
-
-const fullSizePictures = () => {
-  document.body.classList.add('modal-open');
-  container.addEventListener('click', (evt) => {
-    bigPicture.classList.remove('hidden');
-    for (let i = 0; i < photos.length; i++) {
-      if (evt.target.src.includes(photos[i].url)) {
-        bigPictureImg.src = photos[i].url;
-        likesCount.textContent = photos[i].likes;
-        socialCommentsShownCount.textContent = photos[i].comments.length;
-        socialCommentsTotalCount.textContent = photos[i].comments.length;
-        socialCaption.textContent = photos[i].description;
-        socialComments.innerHTML = '';
-        const socialCommentsFragment = document.createDocumentFragment();
-
-
-        for (let j = 0; j < photos[i].comments.length; j++) {
-          const socialComment = socialCommentTemplate.cloneNode(true);
-          socialComment.querySelector('.social__picture').src = photos[i].comments[j].avatar;
-          socialComment.querySelector('.social__picture').alt = photos[i].comments[j].name;
-          socialComment.querySelector('.social__text').textContent = photos[i].comments[j].message;
-          socialCommentsFragment.append(socialComment);
-        }
-
-
-        socialComments.append(socialCommentsFragment);
-
-      }
-    }
-    socialCommentCount.classList.add('hidden');
-    commentsLoader.classList.add('hidden');
-    document.addEventListener('keydown', onEscapeCloseModal);
-    document.addEventListener('click', onLeftClickCloseModal);
-    bigPictureCancelButton.addEventListener('click', onClickCloseModal);
-
-    if (isHidden(bigPicture)) {
-      bigPictureCancelButton.removeEventListener('click', onClickCloseModal);
-    }
-  });
+const onBigPictureCancelClick = () => {
+  closeBigPicture();
 };
 
-function onClickCloseModal(evt) {
-  evt.preventDefault();
-  bigPicture.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-}
-
-function onEscapeCloseModal(evt) {
-  evt.preventDefault();
+const onEscKeyClick = (evt) => {
   if (evt.key === 'Escape') {
-    bigPicture.classList.add('hidden');
-    document.body.classList.remove('modal-open');
+    closeBigPicture();
   }
 
-}
-function onLeftClickCloseModal(evt) {
-  evt.preventDefault();
-  if (evt.target === overlayPicture) {
-    bigPicture.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  }
+};
 
+function closeBigPicture(){
+  bigPicture.classList.add('hidden');
+  bigPictureCancelButton.removeEventListener('click', onBigPictureCancelClick);
 }
 
+const openBigPicture = (pictureId) => {
+  const currentPicture = photos.find((photo) => photo.id === Number(pictureId));
+  const socialCommentsFragment = document.createDocumentFragment();
 
-export{fullSizePictures};
+  bigPictureImg.src = currentPicture.url;
+  likesCount.textContent = currentPicture.likes;
+  socialCommentsTotalCount.textContent = currentPicture.comments.length;
+  socialCommentsShownCount.textContent = quantityOfShownComments;
+  socialComments.innerHTML = '';
 
+  currentPicture.comments.forEach((comment) => {
+    const socialComment = socialCommentTemplate.cloneNode(true);
+
+    socialComment.querySelector('.social__picture').src = comment.avatar;
+    socialComment.querySelector('.social__picture').alt = comment.name;
+    socialComment.querySelector('.social__text').textContent = comment.message;
+    socialCommentsFragment.appendChild(socialComment);
+  });
+  socialComments.appendChild(socialCommentsFragment);
+  socialCaption.textContent = currentPicture.description;
+  socialCommentsCount.classList.add('hidden');
+  socialCommentsLoader.classList.add('hidden');
+
+  bigPicture.classList.remove('hidden');
+  bigPictureCancelButton.addEventListener('click', onBigPictureCancelClick);
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onEscKeyClick);
+};
+
+function countShowComments () {
+
+  return socialComments.children.length;
+}
+
+export { openBigPicture };
--- 9/js/setup.js
+++ 9/js/setup.js
@@ -1,21 +1,33 @@
-import { getRandomNum } from './utils.js';
-import { getArrayOfID, getArrayOfURL, arrayOfDescriptions, getArrayOfComments, COUNT_OF_PHOTOS, LIKES_MIN, LIKES_MAX} from './data.js';
+// Модуль генерации рабочих данных
 
+import { generateRandomInteger, uniqueID, getRandomIndex } from './utils.js';
+import { descriptions, messages, names, photosID, COUNT_OF_PHOTOS, COUNT_OF_LIKES_MIN, COUNT_OF_LIKES_MAX, COUNT_OF_COMMENTS_MIN, COUNT_OF_COMMENTS_MAX, COUNT_OF_AVATARS } from './data.js';
 
-// Массив объектов
-const getArrayOfObjects = function(){
-  const arrayOfObjects = [];
-  for (let i = 0; i < COUNT_OF_PHOTOS; i++){
-    arrayOfObjects.push({
-      id: getArrayOfID()[i],
-      url: getArrayOfURL()[i],
-      description: arrayOfDescriptions[i],
-      likes: getRandomNum(LIKES_MIN, LIKES_MAX),
-      comments: getArrayOfComments()
-    });
-  }
-  return arrayOfObjects;
-};
-const photos = getArrayOfObjects();
+// Генерация комментариев
 
-export {photos};
+const createComments = () => ({
+  id: uniqueID(),
+  avatar: `img/avatar-${generateRandomInteger(1, COUNT_OF_AVATARS)}.svg`,
+  message: messages[getRandomIndex(messages)],
+  name: names[getRandomIndex(names)],
+});
+
+// Генерация фотографий
+
+const createPhotos = (index) => ({
+  id: photosID[index],
+  url: `photos/${photosID[index]}.jpg`,
+  description: descriptions[index],
+  likes: generateRandomInteger(COUNT_OF_LIKES_MIN, COUNT_OF_LIKES_MAX),
+  comments: Array.from(
+    { length: generateRandomInteger(COUNT_OF_COMMENTS_MIN, COUNT_OF_COMMENTS_MAX) },
+    createComments)
+});
+
+const getPhotos = () => Array.from(
+  { length: COUNT_OF_PHOTOS },
+  (_, index) => createPhotos(index));
+
+const photos = getPhotos();
+
+export { photos };
