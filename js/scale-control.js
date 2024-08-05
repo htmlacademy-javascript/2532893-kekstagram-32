fa115619fc5a0941aae63821721bb29ce37ec668
Yuriy Andreev <andreyev.yury@gmail.com>
2024-08-05 10:48:07 +0300

    filters ready, scaling doesnt working

--- js/scale-control.js
+++ js/scale-control.js
@@ -7,29 +7,21 @@ const COUNT_SIZE_VALUE = 25;
 
 // Поиск элементов
 const scaleSmallerButton = document.querySelector('.scale__control--smaller');
-const scaleBiggerButton = document.querySelector('.scale__control--bigger')
+const scaleBiggerButton = document.querySelector('.scale__control--bigger');
 const scaleControlValue = document.querySelector('.scale__control--value');
 const previewPicture = document.querySelector('.img-upload__preview img');
 
 const scaleInputValue = parseInt(scaleControlValue.value, 10);
-let currentScaleValue = scaleControlValue;
+// let currentScaleValue = scaleControlValue;
 
-scaleSmallerButton.addEventListener('click', (evt) => {
-  if (scaleInputValue >= MIN_SIZE_VALUE && scaleInputValue <= MAX_SIZE_VALUE) {
-    evt.preventDefault();
+const scaleDownImage = () => {
+  previewPicture.style.transform = `scale(calc(${(scaleInputValue - COUNT_SIZE_VALUE) / 100}`;
+  scaleControlValue.value = `${scaleInputValue - COUNT_SIZE_VALUE}%`;
+};
 
-    previewPicture.style.transform = `scale(calc(${(scaleInputValue - COUNT_SIZE_VALUE) / 100}`;
-    scaleControlValue.value = `${scaleInputValue - COUNT_SIZE_VALUE}%`;
-  }
-
-});
-
-scaleBiggerButton.addEventListener('click', (evt) => {
-  if (scaleInputValue >= MIN_SIZE_VALUE && scaleInputValue <= MAX_SIZE_VALUE) {
-    evt.preventDefault();
-
-    previewPicture.style.transform = `scale(calc(${(scaleInputValue + COUNT_SIZE_VALUE) / 100}`;
-    scaleControlValue.value = `${scaleInputValue + COUNT_SIZE_VALUE}%`;
-  }
-
-});
+const scaleUpImage = () => {
+  previewPicture.style.transform = `scale(calc(${(scaleInputValue + COUNT_SIZE_VALUE) / 100}`;
+  scaleControlValue.value = `${scaleInputValue + COUNT_SIZE_VALUE}%`;
+};
+scaleSmallerButton.addEventListener('click', scaleDownImage);
+scaleBiggerButton.addEventListener('click', scaleUpImage);
