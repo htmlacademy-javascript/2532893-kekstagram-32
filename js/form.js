ed89eb4e412ca18330f46e658c8164ea41e5d8cc
Yuriy Andreev <andreyev.yury@gmail.com>
2024-08-07 23:35:13 +0300

    almost working

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 // Если ошибка, то не остаётся самой формы и если ошибка второй раз подряд без перезагрузки страницы, то форма не отправляется
+// Если нет ошибки - та же проблема. И та же проблема в задаче со скейлом изображения
 // Непонятно как сделать проверку п.2.3 - хэштеги необязательны;
 
 // Постоянные константы из ТЗ
@@ -114,58 +115,11 @@ pristine.addValidator(
 // Отправка формы
 
 const successMessage = document.querySelector('#success').content;
-const closeSuccessMessage = () => document.querySelector('.success').classList.add('hidden');
 
-const onButtonCloseSuccessMessage = () => {
-  document.querySelector('.success__button').addEventListener('click', closeSuccessMessage);
-};
-const onEscCloseSuccessMessage = () => {
-  document.body.addEventListener('keydown', (evt) => {
-    if (evt.key === 27) {
-      closeSuccessMessage();
-    }
-  });
-};
-const onLeftClickCloseSuccessMessage = () => {
-  document.addEventListener('click', (evt) => {
-    if (!evt.target.closest('.success__inner')) {
-      closeSuccessMessage();
-    }
-  });
-};
-
-const hideSuccessMessage = () => {
-  onButtonCloseSuccessMessage();
-  onEscCloseSuccessMessage();
-  onLeftClickCloseSuccessMessage();
-};
 
 const unsuccessMessage = document.querySelector('#error').content;
 const closeUnsuccessMessage = () => document.querySelector('.error').classList.add('hidden');
 
-const onButtonCloseUnsuccessMessage = () => {
-  document.querySelector('.error__button').addEventListener('click', closeUnsuccessMessage);
-};
-const onEscCloseUnsuccessMessage = () => {
-  document.body.addEventListener('keydown', (evt) => {
-    if (evt.key === 27) {
-      closeUnsuccessMessage();
-    }
-  });
-};
-const onLeftClickCloseUnsuccessMessage = () => {
-  document.addEventListener('click', (evt) => {
-    if (!evt.target.closest('.error__inner')) {
-      closeUnsuccessMessage();
-    }
-  });
-};
-
-const hideUnsuccessMessage = () => {
-  onButtonCloseUnsuccessMessage();
-  onLeftClickCloseUnsuccessMessage();
-  onEscCloseUnsuccessMessage();
-};
 
 function submitForm(successCallback) {
   uploadForm.addEventListener('submit', (evt) => {
@@ -178,15 +132,60 @@ function submitForm(successCallback) {
         body: formData
 
       }).then(() => successCallback())
-        .then(document.body.append(successMessage.cloneNode(true)))
-        .then(hideSuccessMessage())
-        .catch(document.body.append(unsuccessMessage.cloneNode(true)))
-      .then(hideUnsuccessMessage());
+
+        .then(() => hideSuccessMessage(formData))
+        .catch(() => hideUnsuccessMessage(formData));
+    } else {
+      hideUnsuccessMessage();
     }
-    // hideSuccessMessage();
-    // hideUnsuccessMessage();
+  });
+}
+
+function hideUnsuccessMessage(formData) {
+  document.body.append(unsuccessMessage.cloneNode(true));
+  for (const data of formData) {
+    formData.delete(data);
+
+
+  }
+  document.querySelector('.error__button').addEventListener('click', closeUnsuccessMessage);
+
+  document.body.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      closeUnsuccessMessage();
+    }
+  });
+  document.addEventListener('click', (evt) => {
+    if (!evt.target.closest('.error__inner')) {
+      closeUnsuccessMessage();
+    }
   });
 
 }
+
+
+function hideSuccessMessage(formData) {
+  document.body.append(successMessage.cloneNode(true));
+  for (const data of formData) {
+    formData.delete(data);
+  }
+  document.querySelector('.success__button').addEventListener('click', closeSuccessMessage);
+  document.body.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      closeSuccessMessage();
+    }
+  });
+  document.addEventListener('click', (evt) => {
+    if (!evt.target.closest('.success__inner')) {
+      closeSuccessMessage();
+    }
+  });
+}
+
+
+function closeSuccessMessage() {
+  document.querySelector('.success').classList.add('hidden');
+}
+
 
 export { submitForm, closeForm };
